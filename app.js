fbb8acd98e1ae44aacf8852b5770ea55fe5a7724
Geo-the-Pitbull <92966244+Geo-the-Pitbull@users.noreply.github.com>
2022-06-08 08:35:24 -0500

    commit on master

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(session({
 app.use(flash());
 
 app.use('/Employees', EmployeesRoute);
-app.use('/auth', authRoute);
+//app.use('/auth', authRoute);
 //app.use('/PersonalInfo', PersonalInfoRoute);
 app.use('/', indexRoute);
 
---
+++ routes/PersonalInfo.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var conn = require('../lib/db');
+
+//---Show table
+router.get('/List', function (req, res, next){
+    let varSQL = "SELECT * FROM EmployeePersonalInfos";
+     
+    conn.query(varSQL, function(err, rows)     {
+         if(err){
+             //
+         }else{
+             res.render('PersonalInfos-List',{
+                 data: rows
+             });
+         }
+     });
+ });
